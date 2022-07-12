878cd04c60a4adf12a715e7d0762097db7ff8b8d
jenylin <zuapje@gmail.com>
2022-07-11 20:12:56 -0500

    Se agrega la función initGame() y se crean algunos divs con estilos dentro de ella y también algunos métodos.

--- app.js
+++ app.js
@@ -20,6 +20,53 @@ function getKeyString(x, y) {
     let playerId;
     let playerRef;
 
+    function initGame() {
+        // La siguiente es una referencia a todos los jugadores del juego, en oposición a playerRef, ya que esta última es una referencia sólo nuestra
+        // a la que tenemos acceso para actualizar y escribir. Esta referencia de aquí abajo nos permite ver a otros jugadores en el juego.
+        const allPlayersRef = firebase.database().ref(`players`);
+        // La referencia de abajo nos permite leer todas las monedas que están en el mundo del juego.
+        const allCoins = firebase.database().ref(`coins`);
+
+        // El método de abajo es un "listener" que establece un callback para ejecutarse cuando el valor de este ref cambia.
+        // En otras palabras, cada que un jugador se une o se va, o cada vez que sufre una modificación, este callback se ejecutará.
+        // Esta palabra clave "value" viene de firebase.
+        allPlayersRef.on("value", (snapshot) => {})
+
+        // Se ejecuta cuando un nuevo nodo se agrega al árbol, es decir, cuando un nuevo jugador (nuevo para mí) se une.
+        allPlayersRef.on("child_added", (snapshot) => {
+            // La constante de aquí abajo nos da el objeto que se ve exactamente como el de abajo ({ id: playerId,name, color: randomFromArray(playerColors),
+            //  x: 3,  y: 3, coins: 0, })
+            const addedPlayer = snapshot.val(); // val es un método de los snapshots que viene de las refs de firebase
+            // En la línea de aquí abajo creamos un div
+            const characterElement = document.createElement("div");
+            // En la siguiente línea añadimos algunas clases al div.
+            characterElement.classList.add("Character", "grid-cell");
+            if (addedPlayer.id === playerId){
+                // La siguiente línea permite que tu capa esté sobre las de todos los demás y muestra también una pequeña flecha verde
+                // que indica quién eres en la pantalla.
+                characterElement.classList.add("you");
+            }
+            
+            // El innerHTML de nuestro div tendŕa una sombra
+            // también tendrá el sprite de nuesro personaje
+            // El Character_name_container muestra el texto sobre las cabezas de nuestros personajes. Dentro de este habrá un
+            // espacio donde irá el nombre y también un espacio dorado de texto que contará el número de monedas. Este último lo
+            // iniciaremos como cero.
+            // Finalmente, tendremos un div llamado Character_you_arrow que tendrá un pequeño indicador de qué personaje eres en la pantalla.
+            characterElement.innerHTML = (`
+                <div class = "Character_shadow grid-cell"></div>
+                <div class = "Character_sprite grid-cell"></div>
+                <div class = "Character_name-container">
+                    <span class = "Character_name"></span>
+                    <span class = "Character_coins">0</span>
+                </div>
+                <div class = "Character_you-arrow"></div>
+            `);
+
+
+        })
+    }
+
     // Cuando la app arranque, vamos a escuchar los cambios de auth y vamos a disparar/generar un 
     //cambio en auth ingresando de manera anónima y entonces eso debe disparar la parte del if.
     firebase.auth().onAuthStateChanged((user) => {
@@ -81,6 +128,9 @@ function getKeyString(x, y) {
             
             // La siguiente línea es para remover al usuario de Firebase cuando abandona la página
             playerRef.onDisconnect().remove();
+
+            // Comenzar la funcionalidad del juego, localmente, para nosotros, ahora que estamos "logueados".
+            initGame();
 
         } else {
             //You're logged out
